2b78c96792cf5eacc4997f262a42070bcd3d72a5
djdf-crash <chornobil1986>
2018-07-02 14:10:08 +0300

    fix

--- app/src/main/java/ua/in/ukravto/kb/utils/Constants.java
+++ app/src/main/java/ua/in/ukravto/kb/utils/Constants.java
@@ -2,4 +2,5 @@ package ua.in.ukravto.kb.utils;
 
 public interface Constants {
     String SERVICE_API = "http://vv.microtrade.com.ua/api/v1/";
+//    String SERVICE_API = "http://35.238.11.35:7070/api/v1/";
 }
